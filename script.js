9fc21f7c90e3fc37fdff19f556fe0b9cf96da317
Paulo Pinto <gomescasador@gmail.com>
2023-05-09 00:16:37 +0100

    Botões e telas semi configurados

---
+++ script.js
@@ -0,0 +1,14 @@
+function entrada() {
+    let hoje = new Date()
+/*     console.log(`Entrada ${hoje}`) */
+
+    console.log(localStorage)
+
+}
+
+function saida() {
+    let hoje = new Date()
+    console.log(`Saída ${hoje}`)
+
+
+}
