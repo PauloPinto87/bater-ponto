94cb4be41a44e121ce60c1cf2fb7d52429c1d6e9
Paulo Pinto <gomescasador@gmail.com>
2023-05-10 22:16:10 +0100

    Melhorado a tela de registros e inicio do lançamento manual

---
+++ manualmente.js
@@ -0,0 +1,18 @@
+let registroCarregado = document.getElementById('registroCarregado')
+let idInput = document.getElementById('idDoRegistro')
+
+
+function consultar() {
+    if (idInput === '') {
+        console.log('Vazio')
+    }
+
+/*     if (localStorage.getItem(idInput.value) == null ) {
+        
+    } else {
+        registroCarregado.innerHTML = localStorage.getItem(idInput.value)
+
+        registroCarregado.classList.remove('hide')
+    }   */
+}
+
--- visualizar.js
+++ visualizar.js
@@ -4,15 +4,50 @@ let totalTempoTrabalho = 0
 
 
 for (index = 1;index <= idUltRegEnt; index ++) {
-     registros.innerHTML += `<p>${localStorage.getItem(index)}</p>`
 
-
-    if (((localStorage.getItem(index).substring(localStorage.getItem(index).indexOf('m')+2,)).length) == 2) {
+     registros.innerHTML += `<p>ID:${index} - ${localStorage.getItem(index)}</p>`
+/*     if (((localStorage.getItem(index).substring(localStorage.getItem(index).indexOf('m')+2,)).length) == 2) {
         totalTempoTrabalho = totalTempoTrabalho + Number(((localStorage.getItem(index).substring(localStorage.getItem(index).indexOf('m')+2,))).substring(0, 1))
 
     } else {
         totalTempoTrabalho = totalTempoTrabalho + Number(((localStorage.getItem(index).substring(localStorage.getItem(index).indexOf('m')+2,))).substring(0, 2))
+    } */
+}
+
+
+let somaSegundos = 0
+let somaMinutos = 0
+let somaHoras = 0
+let paragrafos = registros.getElementsByTagName('p')
+
+for (index = 0; index <= registros.childNodes.length-2; index++) {
+    let paragrafo = paragrafos[index].innerText
+
+    if (/(\d+)\/(\d+) - (\d+)h:(\d+)m:(\d+)s/.test(paragrafo)){
+        const [,dia, mes, horas, minutos, segundos] = paragrafo.match(/(\d+)\/(\d+) - (\d+)h:(\d+)m:(\d+)s/);
+
+        somaSegundos += Number(segundos)
+        somaMinutos += Number(minutos)
+        somaHoras += Number(horas)
+
+        if (somaSegundos >= 60) {
+            somaSegundos = somaSegundos - 60
+            somaMinutos += 1
+        }
+        if (somaMinutos >= 60) {
+            somaMinutos = somaMinutos - 60
+            somaHoras += 1
+        }
+
+/*      console.log('Dia:', dia);
+        console.log('Mês:', mes);
+        console.log('Horas:', horas);
+        console.log('Minutos:', minutos);
+        console.log('Segundos:', segundos); */
     }
 }
 
-console.log(totalTempoTrabalho)
+
+let resumoDiv = document.getElementById('resumo')
+
+resumoDiv.innerHTML = `Total de ${somaHoras} horas, ${somaMinutos} minutos e ${somaSegundos}segundos.`
